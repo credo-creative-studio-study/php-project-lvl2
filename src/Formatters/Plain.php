f177f03b9b60c75961afcde666391720f58c1b6e
credo-creative-studio-study <credo.creative.creative.study@gmail.com>
2022-02-27 17:10:20 +0500

    change lib

--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -2,7 +2,7 @@
 
 namespace Differ\Formatters\Plain;
 
-use function Funct\Collection\flattenAll;
+use function Functional\flatten;
 
 function format(array $diff): string
 {
@@ -37,7 +37,7 @@ function format(array $diff): string
             }
         }, $diff);
     };
-    return implode("\n", flattenAll($iter($diff, [])));
+    return implode("\n", flatten($iter($diff, [])));
 }
 
 function prepareValue($value): string
